a7190e12412da796f95c34361bcd92e920ce9626
mingi0416 <mingi0416@gmail.com>
2023-03-08 17:37:38 +0900

    Initial commit

---
+++ src/main/java/com/example/mingi_springboot/MingiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mingi_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mingi_springboot/MingiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mingi_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
